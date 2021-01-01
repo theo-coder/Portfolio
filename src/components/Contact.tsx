175057864612317bbfdd290e5b5623e5fd81e08c
theo <theo@posty.fr>
2021-01-01 18:59:06 +0000

    change cors policy

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,7 +12,10 @@ const Contact: React.FC = () => {
 
     const headers = {
         'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        'Access-Control-Allow-Credentials': true
     }
 
     const handleSubmit = (e: any) => {
